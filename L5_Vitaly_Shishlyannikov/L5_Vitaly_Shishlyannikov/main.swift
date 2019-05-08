d6df8c6e8dc47c4138295bdfd7aa771b20467ed3
Vit_sh <vithits@yandex.ru>
2019-05-08 14:40:26 +0300

    Выполнены задания к уроку 5

---
+++ L5_Vitaly_Shishlyannikov/L5_Vitaly_Shishlyannikov/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  L5_Vitaly_Shishlyannikov
+//
+//  Created by Vitalik on 07.05.2019.
+//  Copyright © 2019 Vit. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
